336080bf15be8aa25352725ba9168829f3e43b82
Fernanda Ishida <fer.ishida@gmail.com>
2024-07-23 17:51:21 -0300

    Projectc created

---
+++ src/main/java/com/fIshida/santander_coders_2024/SantanderCoders2024Application.java
@@ -0,0 +1,13 @@
+package com.fIshida.santander_coders_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderCoders2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderCoders2024Application.class, args);
+	}
+
+}
